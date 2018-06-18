a2bd688332961757a98e03a20721030709ef983a
mona2708 <mona.muenstermann@web.de>
2018-06-18 10:14:57 +0200

    - Removed whatgoeshere file - Added AnyChart Libary for simple statistic activities - Added Settings Activity - Added Statistics Activity - Added Profile Activity - Added layouts for before mentioned activities

--- ANDROID/app/src/main/java/com/example/tankmich/tankmich/MainActivity.java
+++ ANDROID/app/src/main/java/com/example/tankmich/tankmich/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.tankmich.tankmich;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button button = findViewById(R.id.options_button);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                try {
+                    Intent i = new Intent(v.getContext(), SettingsActivity.class);
+                    startActivity(i);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
---
+++ ANDROID/app/src/main/java/com/example/tankmich/tankmich/View_Statistics.java
@@ -0,0 +1,40 @@
+package com.example.tankmich.tankmich;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.anychart.anychart.AnyChart;
+import com.anychart.anychart.AnyChartView;
+import com.anychart.anychart.DataEntry;
+import com.anychart.anychart.Pie;
+import com.anychart.anychart.ValueDataEntry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Mona on 11.06.2018.
+ */
+
+public class View_Statistics extends AppCompatActivity{
+
+    public View_Statistics(){
+        
+    }
+    @Override
+    protected void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        setContentView(R.layout.statistics);
+
+        Pie pie = AnyChart.pie();
+
+        List<DataEntry> data = new ArrayList<>();
+        data.add(new ValueDataEntry("John", 10000));
+        data.add(new ValueDataEntry("Jake", 12000));
+        data.add(new ValueDataEntry("Peter", 18000));
+        pie.setData(data);
+
+        AnyChartView anyChartView = (AnyChartView) findViewById(R.id.any_chart_view);
+        anyChartView.setChart(pie);
+    }
+}
